fbdbe30fff26ece9e7fb99aea1488f9ac2df8999
Daro <twojemail@example.com>
2024-12-08 17:58:31 +0100

    Utworzenie klasy Player z właściwościami Name, Position oraz Score i metodami do ruchu po planszy i aktualizacji punktów

---
+++ BoardGameSimulator/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Player
+{
+    public string Name {get; set;}
+    public int Position {get; set;}
+    public int Score {get; set;}
+
+    public Player(string name, int position, int score)
+    {
+        Name = name;
+        Position = position;
+        Score = score;
+    }
+
+    public void RollDiceAndMove()
+    {
+        Random random = new Random();
+        int steps = random.Next(1, 7);
+        Position += steps;
+        Console.WriteLine($"{Name} przesunął się o {steps} pola i jest teraz na pozycji {Position} ");
+    }
+
+    public void UpdateScore(int points)
+    {
+        Score += points;
+        Console.WriteLine($"{Name} zdobył {points} punktów. Aktualny wynik: {Score} ");
+    }
+}
